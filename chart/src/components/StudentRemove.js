5b11365b506424a5e5b8bd8708fa3e45e77f8b4e
Shumaila Shahebaz Khan <shumailak@cybage.com>
2023-04-02 14:25:57 +0530

    minor changes

--- chart/src/components/StudentRemove.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const StudentRemove = () => {
-  return (
-    <div>StudentRemove</div>
-  )
-}
-
-export default StudentRemove
--- chart/src/pages/Dashboard.js
+++ chart/src/pages/Dashboard.js
@@ -1,15 +1,13 @@
 import React from "react";
-// import Header from "../components/Header/Header";
+
 import Charts from "../components/Charts/Charts";
 import EmpolyeeData from "../components/EmpolyeeData";
 const Dashboard = () => {
   return (
     <>
-      {/* <Header /> */}
-    
       <Charts />
 
-      <EmpolyeeData/>
+      <EmpolyeeData />
     </>
   );
 };
